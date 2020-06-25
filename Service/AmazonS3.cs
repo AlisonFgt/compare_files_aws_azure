63778395f7370a618c5b4c92b9a01f8d9e34f7ba
Alison <alisonipuc@gmail.com>
2020-06-25 14:46:18 -0300

    fix: update AmazonS3 for prefix folder

--- Service/AmazonS3.cs
+++ Service/AmazonS3.cs
@@ -4,7 +4,6 @@ using Amazon.S3.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
 namespace Compare_Files_Aws_Azure.Service
@@ -15,24 +14,34 @@ namespace Compare_Files_Aws_Azure.Service
 
         private static IAmazonS3 client;
 
-        public static async Task<List<string>> ListingObjectsAsync(string bucketName)
+        public static async Task<List<string>> ListObjectsAsync(string bucketName)
         {
-            List<string> Files = new List<string>();            
+            List<string> Files = new List<string>();
             client = new AmazonS3Client(bucketRegion);
 
             try
             {
+                string[] folders = bucketName.Split("/".ToCharArray());
                 ListObjectsV2Request request = new ListObjectsV2Request
                 {
-                    BucketName = bucketName,
+                    BucketName = folders[0],
                     MaxKeys = 1000,
-
+                    Prefix = folders.Length == 1 ? "" : folders[1]
                 };
                 ListObjectsV2Response response;
                 do
                 {
                     response = await client.ListObjectsV2Async(request);
-                    Files.AddRange(response.S3Objects.Select(a => a.Key));
+                    Files.AddRange(
+                        response.S3Objects
+                        .Select(a =>
+                        {
+                            string replaceLeadingSlashAndFolder = a.Key.StartsWith("/") ? a.Key.Substring(1, a.Key.Length - 1) : a.Key;
+                            if (replaceLeadingSlashAndFolder != request.Prefix + "/")
+                                replaceLeadingSlashAndFolder = request.Prefix.Length == 0 ? replaceLeadingSlashAndFolder : replaceLeadingSlashAndFolder.Substring(request.Prefix.Length + 1, replaceLeadingSlashAndFolder.Length - (request.Prefix.Length + 1));
+                            return replaceLeadingSlashAndFolder;
+                        })
+                        .Where(a => !a.EndsWith("/")));
                     request.ContinuationToken = response.NextContinuationToken;
                 } while (response.IsTruncated);
             }
@@ -46,7 +55,7 @@ namespace Compare_Files_Aws_Azure.Service
                 Console.WriteLine("Exception: " + e.Message.ToString());
                 Console.ReadKey();
             }
-            Files.RemoveAll(x => x.EndsWith("/")); // remove folder in the list
+            Console.WriteLine($"AWS completed: {Files.Count()}");
             return Files;
         }
     }
